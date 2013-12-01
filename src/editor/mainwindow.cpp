92dd8973d76621120821a965a0eab71b75e58632
rburdet <rodrigoburdet@gmail.com>
2013-12-01 06:36:07 -0300

    Mensajes lindos de parte del editor

--- src/editor/mainwindow.cpp
+++ src/editor/mainwindow.cpp
@@ -12,12 +12,23 @@ MainWindow::MainWindow(){
 	this->tablero = new Tablero(this->builder);
 	this->boxOpcionesBasicas = new BoxOpcionesBasicas(this->builder,this->tablero);
 	this->menu = new Menu(this->builder);
+	builder->get_widget("quit",close);
 	ProbColumnas probColumnas(this->tablero,this->builder);
 	ProbCeldas probCeldas(this->tablero,this->builder);
+	close->signal_activate().connect(sigc::mem_fun(*this,&MainWindow::cerrar));
 }
 
 Gtk::Window* MainWindow::getMainWindow(){
 	return this->mainWindow;
+}
+
+void MainWindow::cerrar(){
+	Gtk::Window* window = new Window();
+	Gtk::MessageDialog dialog(*window,"Desea cerrar?",false,Gtk::MESSAGE_QUESTION,Gtk::BUTTONS_OK_CANCEL);
+	dialog.set_secondary_text("Asegurese de generar antes de salir");
+	int opc=dialog.run();
+	if (opc==Gtk::RESPONSE_OK)
+		this->mainWindow->hide();
 }
 
 MainWindow::~MainWindow(){
